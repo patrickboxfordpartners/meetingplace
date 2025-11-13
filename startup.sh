443eb515dd90a5dd26864e1e5c97465cd622c3a1
patrickboxfordpartners <patrick@boxfordpartners.com>
2025-11-12 17:56:19 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 pip3 install --upgrade pip --break-system-packages
 pip3 install -r requirements.txt --break-system-packages
-
-
-3. **In Azure Portal**, go to your App Service → Configuration → General settings → Startup Command and add:
-
-/home/site/wwwroot/startup.sh
